19faba6aac148958ec931dc277f34d3e89097462
campusbaby <824377536@qq.com>
2025-04-12 22:44:55 +0800

    添加Google Analytics追踪代码并更新README

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { LanguageProvider } from "./context/LanguageContext";
+import Script from "next/script";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -20,6 +21,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-1X5BLC3CRM"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-1X5BLC3CRM');
+          `}
+        </Script>
+      </head>
       <body
         className={`${inter.variable} antialiased`}
       >
